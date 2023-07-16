db1f3bea2199b505afaecf8cb7ac76dc06da35df
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-07-16 15:58:31 -0500

    feat: :rocket: Creacion de .bat para automatizar la inicializacion y instalacion de dependencias de NODE
    Se crea dos archivos iniciar_node.bat y instalar_dependencias.bat para automatizar a un click la instalacion de las dependencias de node y la inicializacion del mismo. el primer archivo inicia node en la carpeta donde se encuentre y el segundo instala las dependencias usadas actualmente en la carpeta donde se encuentra

---
+++ iniciar_node.bat
@@ -0,0 +1,9 @@
+
+@echo off
+
+REM iniciar node
+
+npm init -y
+
+echo inicializacion completada.
+pause
---
+++ instalar_dependencias.bat
@@ -0,0 +1,9 @@
+@echo off
+
+REM instalar dependencias
+
+npm install -E -D nodemon class-validator class-transformer dotenv express mysql2 reflect-metadata typescript
+
+
+echo Instalación completada.
+pause
